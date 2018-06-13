0540347db1b0e6856c2a7c8530d7bc267071428c
akshshar <akshat31187@gmail.com>
2018-06-12 22:41:37 -0700

    cleaned up .o files and consolidated the examples

--- grpc/cpp/cpp/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-SCRIPT_DIR="$(cd "$(dirname "${0}")"; echo "$(pwd)")"
-
-cd $SCRIPT_DIR/src && make clean
-cd $SCRIPT_DIR/src/tutorial && make clean
-cd $SCRIPT_DIR/src/tutorial/rshuttle && make clean
