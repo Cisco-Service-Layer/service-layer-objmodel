c10f4cb0009afaf4c26823275ed9b88883cb911d
arhashem <arhashem@cisco.com>
2019-12-18 12:50:06 -0800

    Moving gendocs to within slapi repo and adding to gitignore (#86)

--- grpc/xrdocs/scripts/protofy.sh
+++ grpc/xrdocs/scripts/protofy.sh
@@ -3,13 +3,13 @@
 # Copyright (c) 2016 by cisco Systems, Inc. 
 # All rights reserved.
 #
-cp tab_h_2.png ../../../../gen-docs/html/tab_h_2.png
-cp search_m.png ../../../../gen-docs/html/search_m.png
-cp tab_a_3.png ../../../../gen-docs/html/tab_a_3.png
-cp tab_s_2.png ../../../../gen-docs/html/tab_s_2.png
-#cp home.html ../../../../gen-docs/html/index.html
+cp tab_h_2.png ../gen-docs/html/tab_h_2.png
+cp search_m.png ../gen-docs/html/search_m.png
+cp tab_a_3.png ../gen-docs/html/tab_a_3.png
+cp tab_s_2.png ../gen-docs/html/tab_s_2.png
+#cp home.html ../gen-docs/html/index.html
 
-cd ../../../../gen-docs/html/
+cd ../gen-docs/html/
 find *.html -type f -exec sed -i 's/struct /message /g;s/Struct /Message /g;s/Class Documentation/Message Documentation/g;s/Public Attributes/Attributes/' {} \;
 
 
