39b6a370939127b532bdae516ae64afd066cbe6a
Ashish Parasrampuria <ashp@cisco.com>
2022-09-06 13:43:07 -0700

    tidy up

--- grpc/cpp/gen-bindings.sh
+++ grpc/cpp/gen-bindings.sh
@@ -6,6 +6,6 @@
 mkdir -p src/gencpp
 printf "Generating cplusplus bindings..."
 #protoc installed for cpp is newer than one for go/python - based on grpc.io instructions
-/$HOME/.local/bin/protoc -I ../protos --grpc_out=./src/gencpp --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ../protos/*.proto
-/$HOME/.local/bin/protoc -I ../protos --cpp_out=./src/gencpp ../protos/*.proto
+${HOME}/.local/bin/protoc -I ../protos --grpc_out=./src/gencpp --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ../protos/*.proto
+${HOME}/.local/bin/protoc -I ../protos --cpp_out=./src/gencpp ../protos/*.proto
 echo "Done"
