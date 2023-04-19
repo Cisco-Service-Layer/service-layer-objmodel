29399746f4bafaf9fc4a893d5a59886b7eda3aab
Mohamed Ben Zeglam <mbenzegl@cisco.com>
2023-04-19 17:06:15 -0400

    update package

--- grpc/python/src/setup.py
+++ grpc/python/src/setup.py
@@ -1,19 +1,3 @@
-# from setuptools import setup, find_packages
-#
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
-#
-# setup(
-#     name="slapi",
-#     version="0.1.0",
-#     author="SLAPI",
-#     author_email="mbenzeg@cisco.com",
-#     description="A package for SL-API",
-#     long_description=long_description,
-#     long_description_content_type="text/markdown",
-#     url="https://wwwin-github.cisco.com/ios-xr/service-layer-objmodel",
-#     packages=find_packages(),
-# )
 from setuptools import setup
 
 setup()
