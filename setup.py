da577154e4b58b1f3b3791f827802a97730d616e
Mohamed Ben Zeglam <mbenzegl@cisco.com>
2023-04-19 23:31:02 -0400

    try different way to generate package

---
+++ setup.py
@@ -0,0 +1,27 @@
+# from setuptools import setup, find_packages
+#
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+#
+# setup(
+#     name="service-layer-objmodel",
+#     version="0.5.0.0",
+#     author="SLAPI-team",
+#     author_email="sl-api-dev@cisco.com",
+#     description="A package for SL-API",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/Cisco-Service-Layer/service-layer-objmodel",
+#     packages=find_packages(),
+#     install_requires=[
+#         "requests"
+#     ],
+#     classifiers=[
+#         "Programming Language :: Python :: 3"
+#     ],
+# )
+
+
+from setuptools import setup
+
+setup()
