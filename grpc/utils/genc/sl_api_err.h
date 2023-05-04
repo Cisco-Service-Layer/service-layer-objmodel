ac42fadb592c58ddda2406d1d7f1c1ead820b981
Varun Krishnan <varunkr@cisco.com>
2023-05-04 09:47:51 -0700

    added errors for parsing cos on path with primary backup checks

--- grpc/utils/genc/sl_api_err.h
+++ grpc/utils/genc/sl_api_err.h
@@ -449,6 +449,10 @@
     SL_NHLFE_INVALID_MULTIPLE_PRIMARY_SETIDS = 0xa021,\
     /* Paths with the same SET ID are not contiguous. 0xa022 */\
     SL_NHLFE_NON_CONTIGUOUS_SETIDS = 0xa022,\
+    /* Paths with the same elsp value are not contiguous. 0xa023 */\
+    SL_NHLFE_NON_CONTIGUOUS_ELSPS = 0xa023,\
+    /* ELSP values are defined for certain paths and not for others. 0xa024 */\
+    SL_NHLFE_INCONSISTENT_ELSP_ON_PATH = 0xa024,\
     /* !!!  MPLS Label block Error codes */\
     /* Offset for label block errors. 0xb000 */\
     SL_LABEL_BLK_ERR_OFFSET = 0xb000,\
@@ -1220,6 +1224,12 @@
         },\
     {SL_NHLFE_NON_CONTIGUOUS_SETIDS ,\
         " Paths with the same SET ID are not contiguous.  "\
+        },\
+    {SL_NHLFE_NON_CONTIGUOUS_ELSPS ,\
+        " Paths with the same elsp value are not contiguous.  "\
+        },\
+    {SL_NHLFE_INCONSISTENT_ELSP_ON_PATH ,\
+        " ELSP values are defined for certain paths and not for others.  "\
         },\
     {SL_LABEL_BLK_ERR_OFFSET ,\
         " Offset for label block errors.  "\
