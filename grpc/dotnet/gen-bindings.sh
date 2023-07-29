8798a1d538b6bb349d13794a19cd7f54c402e9ae
Yasser Arafat Shahul Hameed <hshahul@cisco.com>
2023-07-29 11:34:33 -0700

    optimized makefile and gen-bindings.sh

--- grpc/dotnet/gen-bindings.sh
+++ grpc/dotnet/gen-bindings.sh
@@ -3,32 +3,27 @@
 # Copyright (c) 2023 by cisco Systems, Inc.
 # All rights reserved.
 #
-DOTNET_ROOT=$(pwd)
 printf "Generating dotnet bindings for ServiceLayer..."
-mkdir -p ${DOTNET_ROOT}/src/gencs
+DOTNET_ROOT=$(pwd)
 cd ${DOTNET_ROOT}/src/gencs
-FILE=./ServiceLayer.csproj
-if [ -f "$FILE" ]; then
-  echo "$FILE exists."
-else
-  echo "$FILE does not exist."
-  dotnet new classlib --output . --name ServiceLayer
-  sed -i "/<\/TargetFramework>/a \
-    \ \ \ \ <OutputType>Library<\/OutputType>\n\
-    <Version>1.0.0<\/Version>\n\
-    <Copyright>Copyright (c) $(date +%Y) by cisco Systems, Inc.<\/Copyright> \
-    " $FILE
-fi
-dotnet add package Grpc
-dotnet add package Grpc.Tools
-dotnet add package Google.Protobuf
+# Restore packages needed by gencs
+dotnet restore
 cd ${DOTNET_ROOT}/../protos
 protoc -I . --csharp_out ${DOTNET_ROOT}/src/gencs --grpc_out ${DOTNET_ROOT}/src/gencs --plugin=protoc-gen-grpc=`which grpc_csharp_plugin` *.proto
-if [ -f "../dotnet/src/gencs/Class1.cs" ]; then
-  rm ../dotnet/src/gencs/Class1.cs
-fi
+#Remove unwanted files
+files=("Class1.cs" "SlHello.cs")
+for file in "${files[@]}"
+do
+  if [ -f "$file" ]
+  then
+    rm "$file"
+    echo "Deleted $file"
+  fi
+done
+#Remove unwanted directories
 rm -rf ../dotnet/src/gencs/obj/
-cd ${DOTNET_ROOT}/tutorial/Quickstart
+rm -rf ../dotnet/src/gencs/bin/
+cd ${DOTNET_ROOT}/src/tutorial/Quickstart
 # Restore packages needed by Quickstart
 dotnet restore
 echo "Done"
