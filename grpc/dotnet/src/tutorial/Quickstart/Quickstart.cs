4228e28f476c1ef7e31981b1618ea7a440db9d10
Yasser Arafat Shahul Hameed <hshahul@cisco.com>
2023-07-30 00:33:08 -0700

    vrf registration validated

--- grpc/dotnet/src/tutorial/Quickstart/Quickstart.cs
+++ grpc/dotnet/src/tutorial/Quickstart/Quickstart.cs
@@ -1,21 +1,27 @@
 using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Linq;
 using Grpc.Core;
 using Grpc.Net.Client;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
 using System.IO;
 using ServiceLayer;
 
+
 public class TestVxlan
 {
-    public Dictionary<string, object> register_vrfs { get; set; }
-    public Dictionary<string, object> get_registered_vrfs { get; set; }
-    public Dictionary<string, object> program_v4_routes { get; set; }
-    public Dictionary<string, object> get_v4_routes { get; set; }
-    public Dictionary<string, object> program_v6_routes { get; set; }
-    public Dictionary<string, object> get_v6_routes { get; set; }
-    public Dictionary<string, object> unregister { get; set; }
+    public Dictionary<string, object> RegisterV4Vrfs { get; set; }
+    public Dictionary<string, object> GetV4Vrfs { get; set; }
+//    public Dictionary<string, object> RegisterV6Vrfs { get; set; }
+//    public Dictionary<string, object> GetV6Vrfs { get; set; }
+//    public Dictionary<string, object> ProgramV4Routes { get; set; }
+//    public Dictionary<string, object> GetV4Routes { get; set; }
+//    public Dictionary<string, object> ProgramV6Routes { get; set; }
+//    public Dictionary<string, object> GetV6Routes { get; set; }
+//    public Dictionary<string, object> Unregister { get; set; }
 }
 
 class SLClient
@@ -68,60 +74,116 @@ class SLClient
 		    break;
             }
         }
+
         if (!string.IsNullOrEmpty(jsonFilePath))
         {
             try
             {
                 var jsonString = File.ReadAllText(jsonFilePath);
-                testData = JsonConvert.DeserializeObject<TestVxlan>(jsonString);
+                Console.WriteLine($"Read JSON: {jsonString}");
+            
+                var settings = new JsonSerializerSettings
+                {
+                    ContractResolver = new DefaultContractResolver
+                    {
+                        NamingStrategy = new SnakeCaseNamingStrategy()
+                    }
+                };
+                testData = JsonConvert.DeserializeObject<TestVxlan>(jsonString, settings);
+            
+                if (testData == null) 
+                {
+                    Console.WriteLine("Deserialization resulted in null object. Check your JSON structure.");
+                }
+                else 
+                {
+                    Console.WriteLine($"Deserialized to object: {JsonConvert.SerializeObject(testData)}");
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"An error occurred: {ex.Message}");
-	        Environment.Exit(1);
+                Environment.Exit(1);
             }
+
         }
         else
         {
             Console.WriteLine("JSON file path is not provided.");
 	    Environment.Exit(1);
         }
+	if(testData == null) 
+	{
+                Console.WriteLine("testData is null");
+	        Environment.Exit(1);
+        }
+
         // Setup the channel and client
-        if (!string.IsNullOrEmpty(serverAddress)) {
-	    AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-            channel = GrpcChannel.ForAddress($"https://{serverAddress}");
+        if (!string.IsNullOrEmpty(serverAddress))
+       	{
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            
+            channel = GrpcChannel.ForAddress($"http://{serverAddress}", 
+                                             new GrpcChannelOptions { Credentials = ChannelCredentials.Insecure });
+            
             try {
                 client = new SLRoutev4Oper.SLRoutev4OperClient(channel);
-            }
-            catch (Exception ex) {
+            } catch (Exception ex) {
                 Console.WriteLine("Error initializing the client: " + ex.Message);
-	        Environment.Exit(1);
+                Environment.Exit(1);
             }
-        } else {
+        }
+        else
+       	{
             Console.WriteLine("Server address is not provided.");
 	    Environment.Exit(1);
         }
+        if (testData.RegisterV4Vrfs != null) 	
+	{
+             // Create and populate the SLVrfRegMsg for registration.
+             var regMsg = new SLVrfRegMsg { Oper = SLRegOp.Register };
+             int purgeInterval = Convert.ToInt32(testData.RegisterV4Vrfs["purge_interval"]);
+             int adminDistance = Convert.ToInt32(testData.RegisterV4Vrfs["admin_distance"]);
+             JArray vrfJArray = (JArray)testData.RegisterV4Vrfs["vrf_list"];
+             List<string> vrfList = vrfJArray.ToObject<List<string>>();
+             
+             foreach (string vrfName in vrfList)
+             {
+                 regMsg.VrfRegMsgs.Add(new SLVrfReg
+                 {
+                     VrfName = vrfName,
+                     AdminDistance = (uint)adminDistance,
+                     VrfPurgeIntervalSeconds = (uint)purgeInterval
+                 });
+             }
 
-        // Register the VRFs
-        // Create and populate the SLVrfRegMsg for registration.
-        var regMsg = new SLVrfRegMsg
-        {   
-            Oper = SLRegOp.Register,
-            VrfRegMsgs =
-            {   
-                new SLVrfReg
-                {   
-                    VrfName = "vxlan_red",
-                    AdminDistance = 100,
-                    VrfPurgeIntervalSeconds = 60
+            // Call the SLRoutev4VrfRegOp RPC
+            var regResponse = await client.SLRoutev4VrfRegOpAsync(regMsg);
+            if (regResponse.StatusSummary.Status == SLErrorStatus.Types.SLErrno.SlSuccess)
+            {
+                Console.WriteLine("Operation was successful");
+            }
+            else if (regResponse.StatusSummary.Status == SLErrorStatus.Types.SLErrno.SlSomeErr)
+            {
+                Console.WriteLine("Operation failed for one or more entries");
+                foreach (var result in regResponse.Results)
+                {
+                    if (result.ErrStatus.Status != SLErrorStatus.Types.SLErrno.SlSuccess)
+                    {
+                        Console.WriteLine($"Operation failed for VRF Name: {result.VrfName} with error: {result.ErrStatus.Status}");
+                    }
                 }
             }
-        };
+            else
+            {
+                Console.WriteLine($"Operation failed with error: {regResponse.StatusSummary.Status}");
+            }
+	}
+	else
+	{
+            Console.WriteLine("testData.RegisterV4Vrfs is null");
+	}
 
-        // Call the SLRoutev4VrfRegOp RPC
-        var regResponse = await client.SLRoutev4VrfRegOpAsync(regMsg);
-
-        // Handle the response (not shown here).
 
         // Create and populate the SLVrfRegMsg for EOF operation.
         var eofMsg = new SLVrfRegMsg
